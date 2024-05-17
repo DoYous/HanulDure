997db4761668d3115bdb2834b545891cda775c97
do0ik <oulwind00@naver.com>
2024-05-17 23:11:03 +0900

    Initial Commit

---
+++ src/main/java/com/hanuldure/project/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.hanuldure.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("hanuldure")
+public class MainController {
+
+    @GetMapping("main")
+    public ModelAndView main(ModelAndView model) {
+
+        model.setViewName("/main/main");
+        return model;
+    }
+
+}
