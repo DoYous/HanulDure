14136113127176e25c9910620ec744f0df31e492
Ahnhyojun <hyojunahn111@naver.com>
2024-05-20 02:25:04 +0900

    CMController

--- src/main/java/com/hanuldure/project/controller/CommunityController.java
+++ src/main/java/com/hanuldure/project/controller/CommunityController.java
@@ -1,20 +1,47 @@
 package com.hanuldure.project.controller;
 
+import com.hanuldure.project.dao.CommunityDAO;
+import com.hanuldure.project.dao.impl.CommunityDAOImpl;
 import com.hanuldure.project.dto.CommunityTO;
+import com.hanuldure.project.service.CommunityService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import java.time.LocalDate;
+import java.util.Date;
 
 @Controller
 @RequestMapping("hanuldure")
 public class CommunityController {
+
+    @Autowired
+    private CommunityDAOImpl communitydaoimpl;
+
+    @PostMapping("add")
+    public ModelAndView add(HttpServletRequest request, ModelAndView model) {
+
+        CommunityTO community = new CommunityTO();
+        community.setBoard_title(request.getParameter("board_title"));
+        community.setBoard_write_date(new Date());
+        community.setBoard_type(request.getParameter("board_type"));
+        community.setBoard_content(request.getParameter("board_content"));
+
+        System.out.println(community);
+
+        communitydaoimpl.insertCommunity(community);
+
+        return model;
+    }
+
+    private final CommunityService communityService;
+
+    @Autowired
+    public CommunityController(CommunityService communityService) {
+        this.communityService = communityService;
+    }
 
     //글 작성
     @GetMapping("community/write")
@@ -32,6 +59,7 @@ public class CommunityController {
         return model;
     }
 
+    //상세페이지
     @GetMapping("community/detail")
     public ModelAndView communitydetail(ModelAndView model) {
 
@@ -45,8 +73,15 @@ public class CommunityController {
         return "redirect:/hanuldure/mainpage";
     }
 
+    //글 목록보기 클릭 시
     @GetMapping("community/gowordlist")
     public String gowordlist() {
         return "redirect:/hanuldure/experiencepage";
+    }
+
+    @PostMapping("community/detail")
+    public String addCommunityPost(@ModelAttribute CommunityTO communityTO) {
+        communityService.addCommunity(communityTO);
+        return "redirect:/community/detail";
     }
 }
