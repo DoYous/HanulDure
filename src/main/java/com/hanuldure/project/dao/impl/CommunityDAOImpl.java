8d0b5ef9200b067c043b54ca77637988b3ec97fc
Ahnhyojun <hyojunahn111@naver.com>
2024-05-20 02:24:37 +0900

    CMDAO

---
+++ src/main/java/com/hanuldure/project/dao/impl/CommunityDAOImpl.java
@@ -0,0 +1,21 @@
+package com.hanuldure.project.dao.impl;
+
+import com.hanuldure.project.dao.CommunityDAO;
+import com.hanuldure.project.dto.CommunityTO;
+import com.hanuldure.project.mapper.CommunityMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CommunityDAOImpl implements CommunityDAO {
+
+    @Autowired
+    public CommunityMapper mapper;
+
+    @Override
+    public int insertCommunity(CommunityTO communityTO) {
+        return mapper.insertCommunity(communityTO);
+    }
+
+}
