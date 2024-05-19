ada090703e11192f4adf86b22d5a75ec6ef520a2
Ahnhyojun <hyojunahn111@naver.com>
2024-05-19 23:01:15 +0900

    #1 커뮤니티 글을 작성할 때 둘 다 자유글 모집글이 둘 다 클릭 되는 경우를 수정해서 하나만 가능하게 변경

--- src/main/resources/static/js/communitywritebtclick.js
+++ src/main/resources/static/js/communitywritebtclick.js
@@ -1,11 +1,14 @@
-function toggleButton(button) {
-    if (button.classList.contains('buttonActive')) {
-        // 이미 선택된 상태라면 원래 색상으로 변경
-        button.classList.remove('buttonActive');
-        button.classList.add('buttonNormal');
+function toggleButton(activeId, inactiveId) {
+    var activeButton = document.getElementById(activeId);
+    var inactiveButton = document.getElementById(inactiveId);
+
+    if (activeButton.classList.contains('buttonActive')) {
+        activeButton.classList.remove('buttonActive');
+        activeButton.classList.add('buttonNormal');
     } else {
-        // 선택되지 않은 상태라면 선택된 색상으로 변경
-        button.classList.remove('buttonNormal');
-        button.classList.add('buttonActive');
+        activeButton.classList.remove('buttonNormal');
+        activeButton.classList.add('buttonActive');
+        inactiveButton.classList.remove('buttonActive');
+        inactiveButton.classList.add('buttonNormal');
     }
 }
