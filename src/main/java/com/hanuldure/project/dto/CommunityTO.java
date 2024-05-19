b26dae5f61643109654a25df1f73e8772c94c154
Ahnhyojun <hyojunahn111@naver.com>
2024-05-20 02:24:32 +0900

    CMTO

--- src/main/java/com/hanuldure/project/dto/CommunityTO.java
+++ src/main/java/com/hanuldure/project/dto/CommunityTO.java
@@ -1,72 +1,29 @@
 package com.hanuldure.project.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
 public class CommunityTO {
 
     private int user_seq;
-
     private int board_seq;
     private String board_title;
     private String board_content;
-    private String board_write_date;
-    private char board_type;
+    private Date board_write_date;
+    private String board_type;
 
     public CommunityTO() {
     }
 
-    public CommunityTO(int user_seq, int board_seq, String board_title, String board_content, String board_write_date, char board_type) {
+    public CommunityTO(int user_seq, int board_seq, String board_title, String board_content, String board_write_date, String board_type) {
         this.user_seq = user_seq;
         this.board_seq = board_seq;
         this.board_title = board_title;
         this.board_content = board_content;
-        this.board_write_date = board_write_date;
-        this.board_type = board_type;
-    }
-
-    public int getUser_seq() {
-        return user_seq;
-    }
-
-    public void setUser_seq(int user_seq) {
-        this.user_seq = user_seq;
-    }
-
-    public int getBoard_seq() {
-        return board_seq;
-    }
-
-    public void setBoard_seq(int board_seq) {
-        this.board_seq = board_seq;
-    }
-
-    public String getBoard_title() {
-        return board_title;
-    }
-
-    public void setBoard_title(String board_title) {
-        this.board_title = board_title;
-    }
-
-    public String getBoard_content() {
-        return board_content;
-    }
-
-    public void setBoard_content(String board_content) {
-        this.board_content = board_content;
-    }
-
-    public String getBoard_write_date() {
-        return board_write_date;
-    }
-
-    public void setBoard_write_date(String board_write_date) {
-        this.board_write_date = board_write_date;
-    }
-
-    public char getBoard_type() {
-        return board_type;
-    }
-
-    public void setBoard_type(char board_type) {
         this.board_type = board_type;
     }
 
@@ -78,7 +35,7 @@ public class CommunityTO {
                 ", board_title='" + board_title + '\'' +
                 ", board_content='" + board_content + '\'' +
                 ", board_write_date='" + board_write_date + '\'' +
-                ", board_type=" + board_type +
+                ", board_type='" + board_type + '\'' +
                 '}';
     }
 }
