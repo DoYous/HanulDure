f0bf02553d8fe7c255f20ceba86bdf1c35a83d9a
Ahnhyojun <hyojunahn111@naver.com>
2024-05-20 02:24:49 +0900

    CMDAO

---
+++ src/main/java/com/hanuldure/project/dao/CommunityDAO.java
@@ -0,0 +1,9 @@
+package com.hanuldure.project.dao;
+
+import com.hanuldure.project.dto.CommunityTO;
+import java.util.List;
+
+public interface CommunityDAO {
+    int insertCommunity(CommunityTO communityTO);
+
+}
