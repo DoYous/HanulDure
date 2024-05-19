a3b7528f62bc88ee98091bb2c774f4ea1189f226
Ahnhyojun <hyojunahn111@naver.com>
2024-05-20 02:25:39 +0900

    CMconfiguration

--- src/main/java/com/hanuldure/project/config/ContextConfiguration.java
+++ src/main/java/com/hanuldure/project/config/ContextConfiguration.java
@@ -1,8 +1,10 @@
 package com.hanuldure.project.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@MapperScan("com.hanuldure.project")
 @Configuration
 @ComponentScan(basePackages = {"com.hanuldure.project"})
 public class ContextConfiguration {
