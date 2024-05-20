e5d89c61078a5fbb573cefe70000e56b1eb17481
Ahnhyojun <hyojunahn111@naver.com>
2024-05-21 03:08:55 +0900

    MapperScan에서 지정한 하위 구조 I를 전부 bean으로 인식하게 만듬

--- src/main/java/com/hanuldure/project/config/ContextConfiguration.java
+++ src/main/java/com/hanuldure/project/config/ContextConfiguration.java
@@ -4,7 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@MapperScan("com.hanuldure.project")
+@MapperScan("com.hanuldure.project.mapper")
+/* MapperScan에서 지정한 하위 구조 I를 전부 bean으로 인식하게 만듬*/
 @Configuration
 @ComponentScan(basePackages = {"com.hanuldure.project"})
 public class ContextConfiguration {
