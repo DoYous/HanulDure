a12df4956ba1a4a2c7dc16fc9e42935e69c1fbac
Ahnhyojun <hyojunahn111@naver.com>
2024-05-22 04:07:22 +0900

    #25 db 연동중 (오류)

--- src/main/java/com/hanuldure/project/controller/FarmerProfileController.java
+++ src/main/java/com/hanuldure/project/controller/FarmerProfileController.java
@@ -1,40 +1,38 @@
 package com.hanuldure.project.controller;
 
+import com.hanuldure.project.dto.MemberDTO;
 import com.hanuldure.project.model.Farmer;
 import com.hanuldure.project.service.ExpService;
 import com.hanuldure.project.service.FarmerService;
+import com.hanuldure.project.service.ProfileService;
 import com.hanuldure.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/hanuldure") // URL 수정
+@RequestMapping("hanuldure") // URL 수정
 public class FarmerProfileController {
 
-    @Autowired
-    private FarmerService farmerService;
+    private final ProfileService profileService;
 
     @Autowired
-    private ExpService expService;
+    public FarmerProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
 
-    @GetMapping("/farmerprofile") // GET 요청 처리 및 URL 수정
-    public ModelAndView farmerProfile(ModelAndView model) {
-        // 현재 로그인한 농업인의 ID를 얻어온다고 가정하고,
-        String farmerId = "currentFarmerId";
+    @GetMapping("farmerprofile/{user_seq}")
+    public ModelAndView farmerProfile(@PathVariable("user_seq") int user_seq, ModelAndView model) {
+        MemberDTO farmerprofile = profileService.getUserDetailsBySeq(user_seq);
+        model.addObject("farmerprofile", farmerprofile);
 
-        // 해당 ID를 사용하여 농업인 정보를 조회한다.
-        Farmer farmer = farmerService.getFarmerProfile(farmerId); // FarmerService를 이용하여 농업인 정보 조회
+        System.out.println(farmerprofile);
 
-        // 농업인이 참여한 행사 정보를 가져와서 모델에 추가한다.
-        model.addObject("farmer", farmer); // 모델에 농업인 정보 추가
-        model.addObject("exps", expService.getExpsByFarmerId(farmerId)); // 모델에 농업인이 참여한 행사 정보 추가
-
-        // 프로필 페이지 템플릿을 반환한다.
-        model.setViewName("profile/farmerprofile"); // 프로필 페이지 템플릿 지정
-        return model; // 모델 반환
+        model.setViewName("profile/farmerprofile");
+        return model;
     }
 }
---
+++ src/main/java/com/hanuldure/project/mapper/ProfileMapper.java
@@ -0,0 +1,10 @@
+package com.hanuldure.project.mapper;
+
+import com.hanuldure.project.dto.MemberDTO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface ProfileMapper {
+    @Select("SELECT user_phone, user_region, user_intro FROM user WHERE user_seq = #{user_seq}")
+    MemberDTO selectUserDetails(@Param("user_seq") int user_seq);
+}
