347bf8efe396ac76f76da3922389ab3c2d545c98
Ahnhyojun <hyojunahn111@naver.com>
2024-05-22 18:04:10 +0900

    #25

--- src/main/java/com/hanuldure/project/mapper/ProfileMapper.java
+++ src/main/java/com/hanuldure/project/mapper/ProfileMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Select;
 
 public interface ProfileMapper {
 
-    @Select("SELECT user_phone, user_region, user_intro FROM user WHERE user_seq = #{user_seq}")
-    MemberDTO selectUserDetails(int user_seq);
+    @Select("SELECT user_name, user_phone, user_region, user_intro FROM user WHERE user_seq = #{userSeq}")
+    MemberDTO selectUserDetails(int userSeq);
 }
