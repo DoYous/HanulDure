6091e1c619e66c0a9c14fc65d56ff24f8e7e8220
Ahnhyojun <hyojunahn111@naver.com>
2024-05-22 21:08:49 +0900

    리스트로 체험정보를 받아오기 #25

--- src/main/java/com/hanuldure/project/controller/FarmerProfileController.java
+++ src/main/java/com/hanuldure/project/controller/FarmerProfileController.java
@@ -2,6 +2,7 @@ package com.hanuldure.project.controller;
 
 import com.hanuldure.project.dto.MemberDTO;
 import com.hanuldure.project.model.Farmer;
+import com.hanuldure.project.model.dto.ExpDTO;
 import com.hanuldure.project.service.ExpService;
 import com.hanuldure.project.service.FarmerService;
 import com.hanuldure.project.service.ProfileService;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("hanuldure") // URL 수정
@@ -29,11 +33,29 @@ public class FarmerProfileController {
     @GetMapping("farmerprofile/{userSeq}")
     public ModelAndView farmerProfile(@PathVariable("userSeq") int userSeq, ModelAndView model) {
         MemberDTO farmerprofile = profileService.getUserDetailsBySeq(userSeq);
-//        MemberDTO farmerprofile = new MemberDTO();
-//        farmerprofile.setUserSeq(1);
         model.addObject("farmerprofile", farmerprofile);
 
+//        ExpDTO expInfo = profileService.selectExpByUserSeq(userSeq);
+//        if (expInfo != null) {      //null일 경우 정보 안보냄
+//            model.addObject("expInfo", expInfo);
+//        }
+
+        List<ExpDTO> expInfo = profileService.selectAllExpByUserSeq(userSeq);
+        if (expInfo != null) {      //null일 경우 정보 안보냄
+            Date now = new Date();
+            for (ExpDTO exp : expInfo) {
+                if (exp.getExpEnd().before(now)) {
+                    exp.setExpStatus("종료됨");
+                } else {
+                    exp.setExpStatus("진행중");
+                }
+            }
+            model.addObject("expInfo", expInfo);
+        }
+
+        /*확인문*/
         System.out.println(farmerprofile);
+        System.out.println(expInfo);
 
         model.setViewName("profile/farmerprofile");
         return model;
--- src/main/java/com/hanuldure/project/dao/ProfileDAO.java
+++ src/main/java/com/hanuldure/project/dao/ProfileDAO.java
@@ -3,8 +3,11 @@ package com.hanuldure.project.dao;
 import com.hanuldure.project.dto.CommunityTO;
 import com.hanuldure.project.dto.MemberDTO;
 import com.hanuldure.project.mapper.ProfileMapper;
+import com.hanuldure.project.model.dto.ExpDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("profileDAO")
 public class ProfileDAO {
@@ -12,7 +15,16 @@ public class ProfileDAO {
     @Autowired
     private ProfileMapper mapper;
 
-    public MemberDTO selectUserDetails(int user_seq) {
-        return mapper.selectUserDetails(user_seq);
+    public MemberDTO selectUserDetails(int userSeq) {
+        return mapper.selectUserDetails(userSeq);
     }
+
+//    public ExpDTO selectExpDetails(int userSeq) {
+//        return mapper.selectExpDetails(userSeq);
+//    }
+
+    public List<ExpDTO> selectExpAllDetails(int userSeq) {
+        return mapper.selectExpAllDetails(userSeq);
+    }
+
 }
