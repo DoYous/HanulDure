441de4b943c789aeab26d8a51e26636c864c2ec7
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-05-23 01:35:12 +0900

    #32

---
+++ src/main/java/com/hanuldure/project/mapper/FirstPageMapper.java
@@ -0,0 +1,9 @@
+package com.hanuldure.project.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface FirstPageMapper {
+
+//    @Select("SELECT exp_title,exp_addr,exp_price,exp_min,exp_max,exp_img FROM EXP")
+
+}
